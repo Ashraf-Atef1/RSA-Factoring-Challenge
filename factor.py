a46392c120f81d2380ad4c1cf0594e7aff626399
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-22 21:38:55 +0200

    task

--- factor.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-with open(argv[1], "r") as f:
-    for line in f:
-        num = int(line)
-        for i in range(2, num):
-            if not num % i:
-                print(f"{num}={num // i}*{i}")
-                break
