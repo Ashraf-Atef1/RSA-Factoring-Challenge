31e175680e4ef33289600aa2522d7e57b6cdf340
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-28 06:44:49 +0200

    task

--- check-1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-with open("f_nums", "r") as f_nums:
-    for line in f_nums:
-        line = line[:-1]
-        nums = line.split("=")[1].split("*")
-        if int(line.split("=")[0]) != int(nums[0]) * int(nums[1]):
-            print("error")
